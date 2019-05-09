c1b602a4bd79419a88ced769d47bc8a61d5be5c1
Anu Dhagat <anudhagat@gmail.com>
2019-05-08 22:42:35 -0700

    add group info and styles to group-view

--- src/views/groups-view/index.js
+++ src/views/groups-view/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as GroupSvg } from '../../assets/group.svg';
+import './styles.css';
 
 export default function GroupsView(props) {
   const { setViewGroups, groups, setSelectedGroup } = props;
@@ -9,15 +11,31 @@ export default function GroupsView(props) {
   }
 
   return (
-    <div>
-      <div>Groups View</div>
-      {groups.map(group => {
-        return (
-          <button key={group.name} onClick={() => onGroupClick(group.name)}>
-            {group.name}
-          </button>
-        );
-      })}
+    <div className="view">
+      <h2 className="title">Things To Do</h2>
+      <ul className="list">
+        {groups.map(group => {
+          const tasksCompletedText = `${group.numCompleted} of ${
+            group.tasks.length
+          } tasks complete`;
+          return (
+            <li key={group.name} className="listItem">
+              <button
+                onClick={() => onGroupClick(group.name)}
+                className="groupLink"
+              >
+                <span className="groupSvg">
+                  <GroupSvg />
+                </span>
+                <span className="groupInfo">
+                  <span className="groupName">{group.name}</span>
+                  <span className="completedText">{tasksCompletedText}</span>
+                </span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
