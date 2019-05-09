f03b650fc12d647e0e33f54c6006533143f6299a
Anu Dhagat <anudhagat@gmail.com>
2019-05-08 23:52:04 -0700

    add checkbox and styles to toggle complete status

--- src/components/task-item/index.js
+++ src/components/task-item/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskItemIcon from '../task-item-icon';
 import './styles.css';
 
 function TaskItem({ task }) {
   const { task: taskName, completedAt, locked } = task;
-  const taskStyle = locked
-    ? 'locked'
-    : !completedAt
-    ? 'incomplete'
-    : 'completed';
+  const isComplete = completedAt !== null || completedAt !== undefined;
+
+  const [complete, setComplete] = useState(isComplete);
+  const taskStyle = locked ? 'locked' : complete ? 'completed' : 'incomplete';
+
+  function handleCompleteToggle(complete) {
+    // call to backend to change task status
+    // .then (() => ...
+    setComplete(!complete);
+  }
+
   return (
-    <div className="item">
+    <label className="item">
+      <input
+        type="checkbox"
+        checked={complete}
+        onClick={() => handleCompleteToggle(complete)}
+      />
+
       <span className="icon">
-        <TaskItemIcon locked={locked} completedAt={completedAt} />
+        <TaskItemIcon locked={locked} completedAt={complete} />
       </span>
       <div className={taskStyle}>{taskName}</div>
-    </div>
+    </label>
   );
 }
 
